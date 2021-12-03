00824ce09e453663e456fca729ee9b08aff98118
lostlang <53492781+lostlang@users.noreply.github.com>
2021-12-04 01:54:31 +0300

    Add message list

---
+++ src/main/kotlin/ru/sber/spyfallBot/command/MessageList.kt
@@ -0,0 +1,8 @@
+package ru.sber.spyfallBot.command
+
+// Используйте #1 для нумерцаии данных для вставки
+// И string.replace() для самой вставки
+
+enum class MessageList (val text: String) {
+    START("Приветсвенное сообщение"),
+}
