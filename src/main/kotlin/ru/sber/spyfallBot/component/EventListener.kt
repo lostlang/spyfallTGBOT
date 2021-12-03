bd05779231a292b5c9c8a6f01a18ced85175f608
lostlang <53492781+lostlang@users.noreply.github.com>
2021-12-04 01:47:37 +0300

    Add event listener

---
+++ src/main/kotlin/ru/sber/spyfallBot/component/EventListener.kt
@@ -0,0 +1,45 @@
+package ru.sber.spyfallBot.component
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Lazy
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import ru.sber.spyfallBot.command.CommandList
+import ru.sber.spyfallBot.event.CommandEvent
+import ru.sber.spyfallBot.services.MessageService
+
+@Component
+class EventListener(
+    private val bot: Bot,
+    private val messageService: MessageService
+) {
+
+    inner class CommandEventListener {
+        @EventListener
+        fun onApplicationEvent(event: CommandEvent) {
+            var sendMessage: SendMessage? = null
+
+            when (event.command) {
+                CommandList.START -> {
+                    sendMessage = messageService.simpleTextMessage(event.chatId, "run")
+                }
+                CommandList.RUN -> {
+                    println("run")
+                }
+                CommandList.JOIN -> {
+                    println("join")
+                }
+                CommandList.LEAVE -> {
+                    println("leave")
+                }
+            }
+
+            bot.execute(sendMessage)
+        }
+    }
+
+    @Bean
+    @Lazy
+    fun commandEventListenerBean(): CommandEventListener = CommandEventListener()
+}
