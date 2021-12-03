c3ed98842ab68513f333ecacb405c552b965a056
lostlang <53492781+lostlang@users.noreply.github.com>
2021-12-03 23:02:39 +0300

    Echo bot variation

---
+++ src/main/kotlin/ru/sber/spyfallBot/component/Bot.kt
@@ -0,0 +1,31 @@
+package ru.sber.spyfallBot.component
+
+import org.springframework.stereotype.Component
+import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException
+
+@Component
+class Bot(
+    private val botProperties: BotProperties,
+): TelegramLongPollingBot() {
+
+    override fun getBotUsername() = botProperties.username
+
+    override fun getBotToken() = botProperties.token
+
+    override fun onUpdateReceived(update: Update) {
+        try {
+            execute(
+                SendMessage
+                .builder()
+                .chatId(update.message.chatId.toString())
+                .text("${update.message.chatId}, ${botProperties.username}: ${update.message.text}")
+                .build()
+            )
+        } catch (e: TelegramApiException) {
+            e.printStackTrace()
+        }
+    }
+}
