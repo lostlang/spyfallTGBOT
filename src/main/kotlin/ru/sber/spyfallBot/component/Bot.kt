17298e3e2c5cb042d0b74f8dceb1abdae9b63b5c
lostlang <53492781+lostlang@users.noreply.github.com>
2021-12-04 00:22:52 +0300

    Update bot from merge

--- src/main/kotlin/ru/sber/spyfallBot/component/Bot.kt
+++ src/main/kotlin/ru/sber/spyfallBot/component/Bot.kt
@@ -2,6 +2,7 @@ package ru.sber.spyfallBot.component
 
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
@@ -12,28 +13,29 @@ import javax.annotation.PostConstruct
 class Bot (
     private val botProperties: BotProperties,
     private val botCommands: List<IBotCommand>
-): TelegramLongPollingBot() {
+): TelegramLongPollingCommandBot() {
 
     override fun getBotUsername() = botProperties.username
 
     override fun getBotToken() = botProperties.token
 
-    override fun onUpdateReceived(update: Update) {
-        try {
-            execute(
-                SendMessage
-                .builder()
-                .chatId(update.message.chatId.toString())
-                .text("${update.message.chatId}, ${botUsername}: ${update.message.text}")
-                .build()
-            )
-        } catch (e: TelegramApiException) {
-            e.printStackTrace()
-        }
+    override fun processNonCommandUpdate(update: Update?) {
+
     }
 
     @PostConstruct
     fun initCommands() {
+        botCommands.forEach {
+            register(it)
+        }
 
+        registerDefaultAction { absSender, message ->
+
+            val commandUnknownMessage = SendMessage()
+            commandUnknownMessage.chatId = message.chatId.toString()
+            commandUnknownMessage.text = "Command '${message.text}' unknown"
+
+            absSender.execute(commandUnknownMessage)
+        }
     }
 }
