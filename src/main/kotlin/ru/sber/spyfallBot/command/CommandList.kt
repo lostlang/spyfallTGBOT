f154f4a1763babe4c8b269cadbd44899e14ad765
lostlang <53492781+lostlang@users.noreply.github.com>
2021-12-04 01:45:17 +0300

    Command list update

--- src/main/kotlin/ru/sber/spyfallBot/command/CommandList.kt
+++ src/main/kotlin/ru/sber/spyfallBot/command/CommandList.kt
@@ -4,5 +4,5 @@ enum class CommandList(val command: String, val description: String) {
     START ("start", "Начало работы с ботом"),
     RUN("run", "Найти игру"),
     JOIN("join", "Подключится к игре"),
-    END("leave", "Покинуть игру")
+    LEAVE("leave", "Покинуть игру")
 }
