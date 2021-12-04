511cd11aa23fd06c7d6eddb1575b2e88b609d100
lostlang <53492781+lostlang@users.noreply.github.com>
2021-12-04 21:15:43 +0300

    Complete start bot

--- src/main/kotlin/ru/sber/spyfallBot/component/command/StartCommand.kt
+++ src/main/kotlin/ru/sber/spyfallBot/component/command/StartCommand.kt
@@ -7,11 +7,25 @@ import org.telegram.telegrambots.meta.bots.AbsSender
 import ru.sber.spyfallBot.command.CommandList
 import ru.sber.spyfallBot.command.AbstractCommand
 import ru.sber.spyfallBot.command.MessageList
+import ru.sber.spyfallBot.entity.Player
+import ru.sber.spyfallBot.logic.*
 
 @Component
 class StartCommand: AbstractCommand(CommandList.START) {
     override fun execute(absSender: AbsSender, user: User, chat: Chat, arguments: Array<out String>) {
-        val outText: String = MessageList.START.text
+        val outText: String
+        if (!playerRepository.findByTelegramId(chat.id).isPresent) {
+            outText = formatMessage(MessageList.START.text,
+                arrayOf(user.firstName, user.lastName)
+            )
+
+            playerRepository.save(
+                Player(telegramId = user.id)
+            )
+        } else {
+            outText = MessageList.START_ALREADY_AVAILABLE.text
+        }
+
         sendEvent(chat.id, arrayOf(outText))
     }
 }
