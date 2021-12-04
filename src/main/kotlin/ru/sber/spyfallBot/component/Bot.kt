ff9081f560e8a435c4fdcab78b005cb8f664e0db
lostlang <53492781+lostlang@users.noreply.github.com>
2021-12-05 01:58:38 +0300

    Update non command update

--- src/main/kotlin/ru/sber/spyfallBot/component/Bot.kt
+++ src/main/kotlin/ru/sber/spyfallBot/component/Bot.kt
@@ -4,22 +4,25 @@ import org.springframework.stereotype.Component
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand
 import org.telegram.telegrambots.meta.api.objects.Update
+import ru.sber.spyfallBot.command.CommandInfo
 import ru.sber.spyfallBot.command.CommandMessage
 import ru.sber.spyfallBot.logic.*
+import ru.sber.spyfallBot.services.RedirectService
 import javax.annotation.PostConstruct
 
 @Component
 class Bot (
     private val botProperties: BotProperties,
-    private val botCommands: List<IBotCommand>
+    private val botCommands: List<IBotCommand>,
+    private val receiverService: RedirectService
 ): TelegramLongPollingCommandBot() {
 
     override fun getBotUsername() = botProperties.username
 
     override fun getBotToken() = botProperties.token
 
-    override fun processNonCommandUpdate(update: Update?) {
-
+    override fun processNonCommandUpdate(update: Update) {
+        receiverService.execute(update)
     }
 
     @PostConstruct
@@ -30,7 +33,7 @@ class Bot (
 
         registerDefaultAction { absSender, message ->
             simpleTextMessage(message.chatId,
-                arrayOf(formatMessage(CommandMessage.UNDEFINE.text, arrayOf(message.text)))
+                listOf(formatMessage(CommandMessage.UNDEFINE.text, arrayOf(message.text)))
             ).forEach {
                 absSender.execute(
                     it
