a6a8ce2db2578e28e24ee4c189d59b39aafaa512
lostlang <53492781+lostlang@users.noreply.github.com>
2021-12-04 03:41:23 +0300

    Update columns

--- src/main/kotlin/ru/sber/spyfallBot/entity/Game.kt
+++ src/main/kotlin/ru/sber/spyfallBot/entity/Game.kt
@@ -9,15 +9,25 @@ data class Game (
     @Id
     val id: Long? = null,
 
-    var location: String? = null,
-
     val timeStart: LocalDateTime = LocalDateTime.now(),
 
     val timeForPlay: Long? = null,
+
+    var timePreviousStep: LocalDateTime = LocalDateTime.now(),
 
     @OneToOne
     val spy: Player? = null,
 
     @OneToMany(fetch = FetchType.EAGER)
-    val players: MutableList<Player> = mutableListOf()
-)
+    val players: MutableList<Player> = mutableListOf(),
+
+    @OneToOne
+    var location: Location? = null,
+
+    @OneToMany(fetch = FetchType.EAGER)
+    val locationPool: MutableList<Location> = mutableListOf()
+) {
+    fun nextStep() {
+        timePreviousStep = LocalDateTime.now()
+    }
+}
