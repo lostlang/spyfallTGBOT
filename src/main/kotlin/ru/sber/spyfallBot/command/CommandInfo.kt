60cdb82e0b35bd915a715168699a773f10f4b5c4
lostlang <53492781+lostlang@users.noreply.github.com>
2021-12-05 01:59:51 +0300

    Add help command

--- src/main/kotlin/ru/sber/spyfallBot/command/CommandInfo.kt
+++ src/main/kotlin/ru/sber/spyfallBot/command/CommandInfo.kt
@@ -4,5 +4,6 @@ enum class CommandInfo(val command: String, val description: String) {
     START ("start", "Начало работы с ботом"),
     RUN ("run", "Найти игру"),
     JOIN ("join", "Подключится к игре"),
-    LEAVE ("leave", "Покинуть игру")
+    LEAVE ("leave", "Покинуть игру"),
+    HELP ("help", "Как играть")
 }
