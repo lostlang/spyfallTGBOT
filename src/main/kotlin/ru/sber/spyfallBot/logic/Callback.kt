ccd19c4aaedee1143a4d78511e358977f521a91a
lostlang <53492781+lostlang@users.noreply.github.com>
2021-12-05 03:18:01 +0300

    Add create

--- src/main/kotlin/ru/sber/spyfallBot/logic/Callback.kt
+++ src/main/kotlin/ru/sber/spyfallBot/logic/Callback.kt
@@ -1,12 +1,16 @@
 package ru.sber.spyfallBot.logic
 
 import ru.sber.spyfallBot.command.CommandInfo
+import ru.sber.spyfallBot.component.command.CreateCommand
 import ru.sber.spyfallBot.component.command.HelpCommand
 
 
 fun useCommandClass(command: CommandInfo, arguments: List<String>): List<out String> {
     when (command) {
         CommandInfo.HELP -> return listOf(HelpCommand().getMessage( arguments ))
+        CommandInfo.CREATE -> {
+            return listOf(CreateCommand().getMessage(arguments))
+        }
         else -> return listOf()
     }
 }
